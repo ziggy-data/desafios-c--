db7cb29e354259b7306c085c4fb5d9578324c121
reinaldo <reinaldo.ab00@gmail.com>
2022-02-22 15:54:45 -0300

    inserindo classficacao-triangulo

--- apostila1/classifica-triangulo.cpp
+++ apostila1/classifica-triangulo.cpp
@@ -1,1 +1,24 @@
-# include
+# include <iostream>
+using namespace std;
+
+int main(){
+    int l1,l2,l3;
+
+    cout<<"Indique o primeiro lado"<<endl;
+    cin>>l1;
+    cout<<"Indique o segunda lado"<<endl;
+    cin>>l2;
+    cout<<"Indique o terceiro lado"<<endl;
+    cin>>l3;
+
+    if(l1==l2 && l1==l3 && l2==l3 ){
+        cout<<"O triangulo é equilátero";
+    }
+    else if(l1 == l2 || l1 == l3 || l2 == l3){
+        cout<<"O triangulo é isoceles";
+    }else {
+        cout<<"O triangulo é escaleno";
+    }
+
+    return 0;
+}
