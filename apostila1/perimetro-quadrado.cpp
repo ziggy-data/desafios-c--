44582785aa1d7bd9c2c364b09b0701e86f846f0d
reinaldo <reinaldo.ab00@gmail.com>
2022-02-22 16:48:11 -0300

    incluindo perimetro-quadrado

---
+++ apostila1/perimetro-quadrado.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    //Calcula o peímetro do quadrado
+
+    int perimetro, lado;
+
+    cout<<"Digite o lado do quadrado:"<<endl;
+    cin>>lado;
+
+    perimetro = 4 * lado;
+
+    cout<<"O perímetro é: "<<perimetro<<endl;
+
+}
