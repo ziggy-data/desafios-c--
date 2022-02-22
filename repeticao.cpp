8c26a3ec086a500215a3a6adc21bda5a4f84f788
reinaldo <reinaldo.ab00@gmail.com>
2022-02-21 22:27:25 -0300

    inserindo for

---
+++ repeticao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Introdução de estrutura de repetição demonstrando numeros pares até 1000
+
+    int i;
+
+    for(i=0;i<=1000;i++){
+        if(i % 2 == 0){
+            cout<<i<<endl;
+        }
+    }
+}
