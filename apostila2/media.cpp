5517b407390174d218267bf608bac39dfbdaa9b7
reinaldo <reinaldo.ab00@gmail.com>
2022-02-23 17:35:28 -0300

    comecando apostila2

---
+++ apostila2/media.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n1,n2,resultado;
+
+    cout<<"Digite o primeiro numero"<<endl;
+    cin>>n1;
+    cout<<"Digite o segundo numero"<<endl;
+    cin>>n2;
+
+    resultado = (n1+n2)/2;
+
+    cout<<"Média : "<<resultado<<endl;
+
+    return 0;
+}
---
+++ apostila2/soma-diferenca.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n1,n2,soma,diff;
+
+    cout<<"Digite o primeiro numero"<<endl;
+    cin>>n1;
+    cout<<"Digite o segundo numero"<<endl;
+    cin>>n2;
+
+    soma = n1+n2;
+    diff = n1-n2;
+
+       cout<<"A soma é:"<<soma<<endl;
+       cout<<"A soma é:"<<diff<<endl;
+
+    return 0;
+}
