229067132abdf995a6de6148d640af67ebc1c688
reinaldo <reinaldo.ab00@gmail.com>
2022-02-27 01:39:07 -0300

    ate exercicio 9

---
+++ apostila3/verifica-nome-com-a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    string a;
+
+    cout<<"Testa se o nome possui a primeira letra com 'a' se seim imprime se nao não imprime"<<endl;
+    cout<<"Qual é o seu nome?"<<endl;
+    cin>>a;
+
+    if (a[0] == 'a' || a[0] == 'A' ){
+        cout<<"Seu nome é :"<<a<<endl;
+    }
+    else{
+        cout<<"Seu nome não será impresso"<<endl;
+    }
+
+return 0;
+
+}    
