741ad571a144198e46fd1d69e003f3e0239d39a8
Reinaldo A Simoes <reinaldo.ab00@gmail.com>
2022-04-25 22:06:08 -0300

    tentativa prova

---
+++ av1/compara_numeros_com_0.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void compara_numeros_positivo_negativo(float a ,float b){
+
+    int contador_a = 0, contador_b = 0,guarda_a=a,guarda_b=b;
+
+    if(a>0){
+        while(a>0){
+            a--;
+            contador_a++;
+        }
+    }else{
+        while(a<0){
+        a++;
+        contador_a++;
+        }
+    }
+
+    if(b>0){
+        while(b>0){
+            b--;
+            contador_b++;
+        }
+    }else{
+        while(b<0){
+            b++;
+            contador_b++;
+        }
+    }
+
+    a = guarda_a;
+    b = guarda_b;
+
+    if(contador_a>contador_b){
+        cout<<"O maior valor é: "<<a<<endl;
+    }else{
+        cout<<"O maior valor é: "<<b<<endl;
+    }
+}
+
+int main(){
+
+    int a = 10, b =20;
+
+    compara_numeros_positivo_negativo(a, b);
+
+    return 0;
+}
+
